7b0e22172723422dc6514d7235d348e40b1a3c15
BeatrizBacelar <beabacelar18@gmail.com>
2021-03-25 12:53:22 -0300

    Feat: Construção do HTML e JS do exercício tabuada(17).

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function tabuada(){
+    let num = document.querySelector('input#txtnum')
+    let tab = document.getElementById('selTab')
+
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+     } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        }
+    }
